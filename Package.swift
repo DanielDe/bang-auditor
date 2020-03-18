146a62ce6f7e10e2051371e5116cca1f9309ec95
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2020-03-18 13:15:40 -0700

    support directories of files

--- Package.swift
+++ Package.swift
@@ -6,14 +6,15 @@ import PackageDescription
 let package = Package(
     name: "bang-auditor",
     dependencies: [
-      .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0"))
+      .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
+      .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "bang-auditor",
-            dependencies: ["SwiftSyntax"]),
+            dependencies: ["SwiftSyntax", "Rainbow"]),
         .testTarget(
             name: "bang-auditorTests",
             dependencies: ["bang-auditor"]),
--- Sources/bang-auditor/main.swift
+++ Sources/bang-auditor/main.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftSyntax
+import Rainbow
 
 fileprivate extension Trivia {
     var containsNewline: Bool {
@@ -132,15 +133,54 @@ class BangAuditorVisitor: SyntaxRewriter {
     }
 }
 
-print("")
-let filePath = "./Sources/bang-auditor/test-file.swift"
-let sourceFile = try SyntaxParser.parse(URL(fileURLWithPath: filePath))
-let bangVisitor = BangAuditorVisitor()
-let _ = bangVisitor.visit(sourceFile)
+func isDirectory(path: String) -> Bool {
+    try! URL(fileURLWithPath: filePaths.first!).resourceValues(forKeys: [.isDirectoryKey]).isDirectory!
+}
 
-let fileContents = try! String(contentsOf: URL(fileURLWithPath: filePath))
-let fileLines = fileContents.split(separator: "\n", omittingEmptySubsequences: false)
-bangVisitor.detectedLineNumbers.forEach { lineNumber in
-    print("\(filePath)::\(lineNumber)")
-    print("\n    \(fileLines[lineNumber - 1].trimmingCharacters(in: .whitespacesAndNewlines))\n")
+func normalizeDirectoryPath(_ path: String) -> String {
+    guard path.hasSuffix("/") else {
+        return path
+    }
+
+    return String(path.dropLast(1))
+}
+
+let filePaths = Array(CommandLine.arguments.dropFirst())
+guard !filePaths.isEmpty else {
+    print("You must specify at least one path")
+    exit(1)
+}
+
+filePaths.forEach { filePath in
+    var swiftFilePaths: [String] = []
+
+    if isDirectory(path: filePath) {
+        let normalizedPath = normalizeDirectoryPath(filePath)
+        if let enumerator = FileManager.default.enumerator(atPath: normalizedPath) {
+            for case let path as String in enumerator {
+                if path.hasSuffix(".swift") {
+                    swiftFilePaths.append("\(normalizedPath)/\(path)")
+                }
+            }
+        }
+    } else {
+        swiftFilePaths.append(filePath)
+    }
+
+    swiftFilePaths.forEach { swiftFilePath in
+        let sourceFile = try! SyntaxParser.parse(URL(fileURLWithPath: swiftFilePath))
+        let bangVisitor = BangAuditorVisitor()
+        let _ = bangVisitor.visit(sourceFile)
+
+        let fileContents = try! String(contentsOf: URL(fileURLWithPath: swiftFilePath)) // bang-auditor:ignore
+        let fileLines = fileContents.split(separator: "\n", omittingEmptySubsequences: false)
+
+        if !bangVisitor.detectedLineNumbers.isEmpty {
+            print("\(swiftFilePath)".bold)
+            bangVisitor.detectedLineNumbers.forEach { lineNumber in
+                print("    line \(lineNumber)".red)
+                print("        \(fileLines[lineNumber - 1].trimmingCharacters(in: .whitespacesAndNewlines))")
+            }
+        }
+    }
 }
